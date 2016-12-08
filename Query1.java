fd27ce8e8f253b71001fa65f17f2a2ce25d755f1
KevinAiken <me@kevinaiken.com>
2016-12-07 22:53:10 -0500

    Updated Query1. Project method appears not to be fully functional.

--- Query1.java
+++ Query1.java
@@ -4,14 +4,21 @@ public class Query1 {
   public static void main(String args[]) {
   Database db = new Database();
   db.initializeDatabase(args[0]);
-  db.displaySchema();
+
   Relation r1 = db.getRelation("EMPLOYEE");
-  Relation r2 = db.getRelation("DEPARTMENT");
-  ArrayList<String> cnames = new ArrayList<String>();
-  cnames.add("DNUMBER");
-  Relation r3 = r1.project(cnames);
-  System.out.println(r3);
-  System.out.println(r1);
-  System.out.println(r2);
+  //Relation r2 = db.getRelation("DEPARTMENT");
+  Relation t1 = r1.select("col", "DNO", "=", "num", "6" );
+  //System.out.println(t1);
+  //Relation t2 = r1.join(t1);
+  ArrayList<String> attr = new ArrayList<String>();
+  attr.add("FNAME");
+//  attr.add("LNAME");
+//  attr.add("ADDRESS");
+
+  Relation result = t1.project(attr);
+  result.setName("ANSWER");
+  System.out.println(result);
+  System.out.println(t1);
+
   }
 }
